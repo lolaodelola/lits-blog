496a72e4f39edb1b5a55c9a433b0682f6b5a4601
lolaodelola <damzcodes@gmail.com>
2019-09-28 15:29:47 +0100

    uncomment everything

--- podcast.rb
+++ podcast.rb
@@ -26,41 +26,41 @@ s3_resource = Aws::S3::Resource.new(region: 'eu-west-2', credentials: credential
 s3_client = Aws::S3::Client.new(region: 'eu-west-2', credentials: credentials)
 transcribe_client = Aws::TranscribeService::Client.new(region: 'eu-west-2', credentials: credentials)
 
-# # Create audio object in S3
-# puts "Upload audio to S3"
-# File.open(@audio[:path], 'rb') do |file|
-#   s3_client.put_object(
-#       acl: 'public-read',
-#       body: file,
-#       bucket: ENV['AWS_BUCKET'],
-#       key: "audio/#{@audio[:name]}.mp3",
-#       metadata: {
-#           duration: @audio[:duration],
-#           sample_rate: @audio[:sample_rate]
-#       }
-#   )
-# end
-# puts "Audio upload complete"
-#
-# @audio[:url] = s3_resource.bucket(ENV['AWS_BUCKET']).object("audio/#{@audio[:name]}.mp3").public_url
-#
-# puts 'Starting transcription job'
-# transcribe_client.start_transcription_job(
-#    transcription_job_name: @audio[:name], # required
-#    language_code: 'en-GB', # required,
-#    media_sample_rate_hertz: @audio[:sample_rate].to_i,
-#    media_format: 'mp3', # required
-#    media: { # required
-#             media_file_uri: @audio[:url]
-#    },
-#    output_bucket_name: ENV['AWS_BUCKET']
-# )
-#
-# s3_client.wait_until(:object_exists, bucket: ENV['AWS_BUCKET'], key: "#{@audio[:name]}.json") do |w|
-#   w.max_attempts = 10
-#   w.delay = 120
-# end
-# puts "Finished transcription job"
+# Create audio object in S3
+puts "Upload audio to S3"
+File.open(@audio[:path], 'rb') do |file|
+  s3_client.put_object(
+      acl: 'public-read',
+      body: file,
+      bucket: ENV['AWS_BUCKET'],
+      key: "audio/#{@audio[:name]}.mp3",
+      metadata: {
+          duration: @audio[:duration],
+          sample_rate: @audio[:sample_rate]
+      }
+  )
+end
+puts "Audio upload complete"
+
+@audio[:url] = s3_resource.bucket(ENV['AWS_BUCKET']).object("audio/#{@audio[:name]}.mp3").public_url
+
+puts 'Starting transcription job'
+transcribe_client.start_transcription_job(
+   transcription_job_name: @audio[:name], # required
+   language_code: 'en-GB', # required,
+   media_sample_rate_hertz: @audio[:sample_rate].to_i,
+   media_format: 'mp3', # required
+   media: { # required
+            media_file_uri: @audio[:url]
+   },
+   output_bucket_name: ENV['AWS_BUCKET']
+)
+
+s3_client.wait_until(:object_exists, bucket: ENV['AWS_BUCKET'], key: "#{@audio[:name]}.json") do |w|
+  w.max_attempts = 10
+  w.delay = 120
+end
+puts "Finished transcription job"
 
 puts 'Fetiching transcript'
 draft_transcript = s3_client.get_object(
