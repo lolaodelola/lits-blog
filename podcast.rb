aeb1a199db2e7daebaffef7e305f0e9becafbe6d
lolaodelola <damzcodes@gmail.com>
2019-09-28 14:55:55 +0100

    refactor podcast script

--- podcast.rb
+++ podcast.rb
@@ -1,68 +1,89 @@
 require 'dotenv/load'
 require 'rb-readline'
+require 'time'
 require 'aws-sdk-s3'
 require 'aws-sdk-transcribeservice'
 require 'pry'
 
-@audio_path = ARGV[0]
-@file_name = ARGV[1]
+@audio = {
+  path: ARGV[0],
+  name: ARGV[1]
+}
 
+# Metadata setup
+puts "Setup metadata"
+get_duration = `mdls -name kMDItemDurationSeconds #{@audio[:path]}`
+duration_seconds = get_duration.gsub('kMDItemDurationSeconds =', '').strip
+@audio[:duration] = Time.at(duration_seconds.to_f).utc.strftime('%H:%M:%S')
 
+get_sample_rate = `mdls -name kMDItemAudioSampleRate #{@audio[:path]}`
+@audio[:sample_rate] = get_sample_rate.gsub('kMDItemAudioSampleRate =', '').strip
+puts "Metadata setup complete"
+
+# AWS setup
 credentials =  Aws::Credentials.new(ENV['AWS_KEY'], ENV['AWS_SID'])
-
-# S3 objects
-s3_resource = Aws::S3::Resource.new(region: "eu-west-2", credentials: credentials)
-s3_client = Aws::S3::Client.new(region: "eu-west-2", credentials: credentials)
-
-# Transcribe object
-aws_transcribe_client = Aws::TranscribeService::Client.new(region: "eu-west-2", credentials: credentials)
+s3_resource = Aws::S3::Resource.new(region: 'eu-west-2', credentials: credentials)
+s3_client = Aws::S3::Client.new(region: 'eu-west-2', credentials: credentials)
+transcribe_client = Aws::TranscribeService::Client.new(region: 'eu-west-2', credentials: credentials)
 
 # Create audio object in S3
-obj = s3_resource.bucket("lits-podcast-episodes").object("audio/#{@file_name}.mp3")
-puts "Uploading audio"
-if obj.upload_file(@audio_path)
-  puts "done"
-  @audio_url = s3_resource.bucket('lits-podcast-episodes').object("audio/#{@file_name}.mp3").public_url
-else
-  puts "nope"
+puts "Upload audio to S3"
+File.open(@audio[:path], 'rb') do |file|
+  s3_client.put_object(
+      acl: 'public-read',
+      body: file,
+      bucket: ENV['AWS_BUCKET'],
+      key: "audio/#{@audio[:name]}.mp3",
+      metadata: {
+          duration: @audio[:duration],
+          sample_rate: @audio[:sample_rate]
+      }
+  )
 end
+puts "Audio upload complete"
 
-puts "Starting transcription job"
-aws_transcribe_client.start_transcription_job({
-                             transcription_job_name: @file_name, # required
-                             language_code: "en-GB", # required,
-                             media_sample_rate_hertz: 44_100,
-                             media_format: "mp3", # required
-                             media: { # required
-                                      media_file_uri: @audio_url,
-                             },
-                             output_bucket_name: 'lits-podcast-episodes'
-})
+@audio[:url] = s3_resource.bucket(ENV['AWS_BUCKET']).object("audio/#{@audio[:name]}.mp3").public_url
 
-s3_client.wait_until(:object_exists, {bucket: 'lits-podcast-episodes', key: "#{@file_name}.json"}) do |w|
+puts 'Starting transcription job'
+transcribe_client.start_transcription_job(
+   transcription_job_name: @audio[:name], # required
+   language_code: 'en-GB', # required,
+   media_sample_rate_hertz: @audio[:sample_rate].to_i,
+   media_format: 'mp3', # required
+   media: { # required
+            media_file_uri: @audio[:url]
+   },
+   output_bucket_name: ENV['AWS_BUCKET']
+)
+
+s3_client.wait_until(:object_exists, bucket: ENV['AWS_BUCKET'], key: "#{@audio[:name]}.json") do |w|
   w.max_attempts = 10
   w.delay = 120
 end
-puts "Fetiching transcript"
-draft_transcript = s3_client.get_object({
-                  bucket: 'lits-podcast-episodes',
-                  key: "#{@file_name}.json"
-})
+puts "Finished transcription job"
+
+puts 'Fetiching transcript'
+draft_transcript = s3_client.get_object(
+  bucket: ENV['AWS_BUCKET'],
+  key: "#{@audio[:name]}.json"
+)
 
 # Parse transcript
-draft_transcript_body = JSON.parse(draft_transcript.body.read)["results"]["transcripts"][0]["transcript"]
-puts "Writing to episode post"
-File.open("source/podcast/episodes/#{@file_name}.html.markdown", "w") do |file|
+draft_transcript_body = JSON.parse(draft_transcript.body.read)['results']['transcripts'][0]['transcript']
+puts 'Writing to episode post'
+File.open("source/podcast/episodes/#{@audio[:name]}.html.markdown", 'w') do |file|
   file.puts "---"
-  file.puts "title: #{@file_name}"
+  file.puts "title: #{@audio[:name]}"
   file.puts "blog: podcast"
   file.puts "published: false"
-  file.puts "date:"
+  file.puts "date:  #{Time.now}"
+  file.puts "duration: #{@audio[:duration]}"
   file.puts "description:"
+  file.puts "audio_link: #{@audio[:url]}"
   file.puts "---"
-  file.puts "<audio controls src='#{@audio_url}'>
+  file.puts "<audio controls src='#{@audio[:url]}'>
       Your browser does not support the <code>audio</code> element.
     </audio>"
-  file.puts "\n"
+  file.puts "\n" # Add double space
   file.puts draft_transcript_body
 end
